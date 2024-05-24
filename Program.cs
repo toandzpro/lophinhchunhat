77f0db3695a4f0cf07b4125034aa1f05898f26a6
toandzpro <kimthoai15620@gmail.com>
2024-05-25 00:49:08 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+public class baitap25
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Enter the width:");
+        double width = Double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the height:");
+        double height = Double.Parse(Console.ReadLine());
+        Rectangle rectangle = new Rectangle(width, height);
+        Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+        Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+        Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+    }
+}
+public class Rectangle
+{
+    double width, height;
+
+    public Rectangle()
+    {
+    }
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double GetArea()
+    {
+        return this.width * this.height;
+    }
+
+    public double GetPerimeter()
+    {
+        return (this.width + this.height) * 2;
+    }
+
+    public String Display()
+    {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
+    
